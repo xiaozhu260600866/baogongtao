e102dc034073a06b514f4132978bf4f34b0382a2
xiaozhu260600866 <260600866@qq.com>
2022-01-21 18:08:55 +0800

    xiaozhu

--- baogongtao_app/pages/integral/lists/main.vue
+++ baogongtao_app/pages/integral/lists/main.vue
@@ -2,93 +2,45 @@
 	<view>
 		<page :parentData="data" :formAction="formAction"></page>
 		<view class="pro-list">
-			<view class="info-header bg-f fs-13 fc-3">
-				<view class="ih-item ptb8" @click="goto('/pages/user/integral/index/index',1)">
-					<view class="num lh-24 mb3 fc-red fs-20 Arial">{{userInfo.integral || 0}}</view>
-					<view class="name">我的积分</view>
-				</view>
-				<view class="ih-item ptb8" @click="goto('/pages/user/integral/list/main',1)">
-					<view class="num lh-24 mb3 fc-red iconfont icon-integral-order fs-20"></view>
-					<view class="name">兑换记录</view>
+			<view class="count-header main-bg">
+				<view class="name fs-18 fc-white">我的积分</view>
+				<view class="count-num Arial">{{data.integral}}</view>
+				<view @click="goto('/pages/user/integral/lists/main',1)" class="record fs-14 fc-white">明细</view>
+			</view>
+			<view class="list-group block-sec mb0">
+				<dx-list-cell arrow name="我的订单" imgSrc="https://boss.doxinsoft.com/images/app/orderlist.png" imgWidth="20" imgHeight="20" imgR="6"
+				 @click="goto('/pages/user/integral/order/new_order/main?status=12&historyUrl=del',1)"></dx-list-cell>
+			</view>
+			<view class="porducts">
+				<view class="pro-div" v-for="item in data.lists.data">
+					<view @click="goto('/pages/integral/show/main?id='+item.id)" class="pro-box">
+						<view class="cms_cover">
+							<image :src="item.firstCover" mode="aspectFill" class="image"/>
+						</view>
+						<view class="cms_title Aname fs-15 nowrap">{{item.name}}</view>
+						<view class="cms_price">
+							<view class="saleprice fs-16">{{item.integral}}<span class="fs-12 fc-9 ml3"> 积分</span></view>
+						</view>
+					</view>
 				</view>
 			</view>
-			<!-- <view class="porducts">
-				<view class="pro-view" v-for="item in lists.lists" @click="goto('/pages/integral/show/main?id='+item.id)">
-					<view class="cms_cover">
-						<image class="img" :src="item.firstCover" mode="aspectFill"/>
-					</view>
-					<view class="cms_title Aname fs-15 nowrap">{{item.name}}</view>
-					<view class="cms_price"><text class="Arial fc-red fs-16 pr3">{{item.integral}}</text> 积分</view>
-				</view>
-			</view> -->
-			<!-- <view class="pro-col">
-				<view class="pro-col-item">
-					<view class="pro-col-box" v-for="(item,index) in lists.lists" :key="index" v-if="(index+1)%2!=0"
-					 @click="goto('/pages/integral/show/main?id='+item.id)">
-						<image :src="item.firstCover" class="img" mode="widthFix" />
-						<view class="content">
-							<view class="tit lh-20">{{item.name}}</view>
-							<view class="priceG lh-22">
-								<text class="price">{{item.integral}}</text>
-								<text class="unit">积分</text>
-							</view>
-						</view>
-					</view>
-				</view>
-				<view class="pro-col-item">
-					<view class="pro-col-box" v-for="(item,index) in lists.lists" :key="index" v-if="(index+1)%2==0"
-					 @click="goto('/pages/integral/show/main?id='+item.id)">
-						<image :src="item.firstCover" class="img" mode="widthFix" />
-						<view class="content">
-							<view class="tit lh-20">{{item.name}}</view>
-							<view class="priceG lh-22">
-								<text class="price">{{item.integral}}</text>
-								<text class="unit">积分</text>
-							</view>
-						</view>
-					</view>
-				</view>
-			</view> -->
-			暂无数据
-			<!-- <hasMore :parentData="data"></hasMore> -->
+			<hasMore :parentData="data"></hasMore>
 		</view>
 	</view>
 </template>
 
 <script>
-	import {userinfo, action, logout} from "@/api/user";
+	import dxListCell from "doxinui/components/list-cell/list-cell"
 	export default {
+		components:{
+			dxListCell
+		},
 		data() {
 			return {
-				formAction: '/shop/integral/lists?type=0',
+				formAction: '/integral/lists?type=0',
 				mpType: 'page', //用来分清父和子组件
 				data: this.formatData(this),
-				getSiteName: this.getSiteName(),
-				userInfo:{},
-				lists:{
-					integral: 2595,
-					lists:[{
-						firstCover:'/static/images/user-bg.jpg',
-						name:'汽车机器人',
-						integral: 1500,
-						id:18
-					},{
-						firstCover:'https://www.baogongtao.com/images/wap/banner01.jpg',
-						name:'越南大青芒玉芒金煌芒精选大果青皮芒果香进口新鲜水果净重9斤',
-						integral: 1500,
-						id:18
-					},{
-						firstCover:'https://www.baogongtao.com/images/wap/banner01.jpg',
-						name:'新鲜红富士苹果水果冰糖心5斤好吃的一整箱陕西平果生鲜萍果 5斤装（净重5斤）',
-						integral: 1500,
-						id:18
-					},{
-						firstCover:'/static/images/user-bg.jpg',
-						name:'国产水蜜桃 新鲜桃子 精选优级果1.5kg装 单果150g以上 生鲜 新鲜水果',
-						integral: 1500,
-						id:18
-					}]
-				},
+				getSiteName: this.getSiteName()
 			}
 		},
 		onReachBottom() {
@@ -102,23 +54,17 @@
 			return this.shareSource(this, '商城');
 		},
 		onLoad(options) {
-			userinfo({token:uni.getStorageSync('token')}).then((res)=>{
-				
-				this.userInfo = res.data.user;
-				
-
-				
-			})
+			this.ajax();
 		},
 		methods: {
 			ajax() {
-				this.getAjax(this).then(msg => {
+				this.getAjaxForApp(this,{token:uni.getStorageSync('token')}).then(msg => {
 					console.log(this.data);
 				});
 			}
 		}
 	}
 </script>
-<style>
-@import "./index.css";
+<style scoped="">
+@import url('index.css');
 </style>
--- baogongtao_app/pages/integral/show/main.vue
+++ baogongtao_app/pages/integral/show/main.vue
@@ -1,53 +1,43 @@
 <template>
 	<view>
 		<page :parentData="data" :formAction="formAction"></page>
-		<view>
+		<view v-if="data.show">
 			<view class="show_banner">
-				<dx-swiper :data="detail.covers" :tbPadding="0" :lrPadding="0" :bdR="0"></dx-swiper>
+				<myswiper :lists="data.covers" purl="product"></myswiper>
 			</view>
 			<view class="pro-info bg-f mb8 plr15 ptb12">
-				<view class="pro-name fs-18 pb15 lh-1_4">{{detail.product.name}}</view>
-				<view class="pro-price lh-1"><text class="Arial fs-24">{{detail.product.integral}}</text><text class="fs-14 fc-9 pl5">积分</text></view>
+				<view class="pro-name fs-18 pb15 lh-1_4">{{data.product.name}}</view>
+				<view class="pro-price lh-1"><text class="fs-24 price">{{data.product.integral}}</text><text class="fs-14 fc-9 pl5">积分</text></view>
 			</view>
 			<view class="pro-title plr15 bd-b bg-f">
 				<text class="name fs-17">商品详情</text>
 			</view>
-			<view class="pro-content p15">
-				<uParse :content="detail.product.content" />
+			<view class="pro-content p15 mb50">
+					<uParse :content="data.product.content" />
 			</view>
-			<view>
-				<dxftButton size="lg" type="info" v-if="detail.integral<detail.product.integral">当前积分为{{detail.integral}}分，不足以兑换此商品哦</dxftButton>
-				<dxftButton size="lg" type="primary" @click="toBuy" v-else>立即兑换</dxftButton>
+			<view id="footer-btn">
+				<dx-button size="lg" type="info" block round v-if="data.integral < data.product.integral ">当前积分为{{data.integral}}分，不足以兑换此商品哦</dx-button>
+				<dx-button size="lg" type="success" block round @click="toBuy" v-else>立即兑换</dx-button>
 			</view>
+
+
 		</view>
 	</view>
 </template>
 
 <script>
+	import "@/components/gaoyia-parse/parse.css";
 	import uParse from '@/components/gaoyia-parse/parse.vue'
-	import dxftButton from "doxinui/components/button/footer-button"
-	import dxSwiper from "doxinui/components/swiper/swiper"
 	export default {
-		components: {uParse,dxftButton,dxSwiper},
 		data() {
 			return {
-				formAction: '/shop/integral/show',
+				
+				formAction: '/integral/show',
 				mpType: 'page', //用来分清父和子组件
 				data: this.formatData(this),
 				getSiteName: this.getSiteName(),
-				detail:{
-					covers:[
-						{cover: "/static/images/news/01.jpg"},
-						{cover: "/static/images/news/02.jpg"},
-					],
-					integral: 2533,
-					product:{
-						name:'新鲜红富士苹果水果冰糖心5斤好吃的一整箱陕西平果生鲜萍果 5斤装（净重5斤）',
-						integral: 1500,
-						content:'<p>新鲜红富士苹果水果冰糖心5斤好吃的一整箱陕西平果生鲜萍果 5斤装（净重5斤）</p>'
-					},
-					order_no: 20210020003,
-				}
+				id:''
+				
 			}
 		},
 		onReachBottom() {
@@ -61,29 +51,33 @@
 			return this.shareSource(this, '商城');
 		},
 		onLoad(options) {
-			//this.ajax();
+			this.id = options.id;
+			this.ajax();
 		},
 		methods: {
 			toBuy() {
 				this.postAjax(this.formAction, {
 					id: this.data.product.id,
+					token:uni.getStorageSync('token'),
 					num: 1,
 					is_info: 0
 				}).then(msg=>{
 					if (msg.data.status == 2) {
-						this.goto("/pages/integral/order/buy?order_no=" + msg.detail.order_no);
+						this.goto("/pages/integral/order/buy/main?order_no=" + msg.data.order_no);
 					}
 				});
 			},
 			ajax() {
-				this.getAjax(this).then(msg => {
+				this.getAjaxForApp(this,{id:this.id,token:uni.getStorageSync('token')}).then(msg => {
 					console.log(this.data);
 				});
 			}
+		},
+		components: {
+			uParse
 		}
 	}
 </script>
-<style lang="scss">
-@import "./index.scss";
-@import "@/components/gaoyia-parse/parse.css";
+<style scoped="">
+@import url('index.css');
 </style>
